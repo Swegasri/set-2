b2b1f79ead6d48eb0c80e4b8ad7e0725e8a57724
Swegasri <48052644+Swegasri@users.noreply.github.com>
2019-08-12 10:40:08 -0700

    b22.C

---
+++ b22.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int number,i,a[10000],k=0;
+    scanf("%d",&number);
+    
+     for(i=0;i<number;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<number;i++)
+    {
+        if(a[i]>s)
+        {
+            k=a[i];
+        }
+         }
+    printf("%d",k);
+    return 0;
+}
