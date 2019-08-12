0de71e4ef1059883b4d684651e8a95005d43506e
Swegasri <48052644+Swegasri@users.noreply.github.com>
2019-08-12 10:09:35 -0700

    b30.C

--- b30.C
+++ b30.C
@@ -2,9 +2,9 @@
 #include<math.h>
 int main()
 {
-int n1,min1,hour2,min2,hour3,min3;
-scanf("%d %d\n%d %d",&n1,&min1,&hour2,&min2);
-hour3=abs(n1-hour2);
+int t1,min1,hour2,min2,hour3,min3;
+scanf("%d %d\n%d %d",&t1,&min1,&hour2,&min2);
+hour3=abs(t1-hour2);
 min3=abs(min1-min2);
 printf("%d %d",hour3,min3);
 return 0;
